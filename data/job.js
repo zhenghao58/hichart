b8d1eb94d0a94516489a8bdf0e26e84556e3df6f
HAO ZHENG <hz837@nyu.edu>
2015-02-10 01:54:08 -0500

    modify the algorithm of the data

--- data/job.js
+++ data/job.js
@@ -1,18 +1,31 @@
-var fillData = require('./data');
 var db = require('../config/db');
 var DataSchema = require('../schema/data');
-var data=[];
+var previous;
+var datePoint = new Date(Date.now()-1000*60*60*1.5);
+DataSchema.findOne().gt('time', datePoint).exec(function(err, previousData){
+     if(err){
+          console.log(err);
+     }
+     else{
+          previous = previousData.data;
+          console.log(previous);
 
-data= fillData(data);
-var record = new DataSchema({
-	data: data[data.length-1]
+          var data = previous + Math.random() * 20 - 10;
+          data = Number(data.toFixed(2));
+
+          var record = new DataSchema({
+               data: data
+          })
+
+          record.save(function(err){
+               if(err){
+                    console.log(err);
+               }
+               else{
+                    var date =new Date();
+                    console.log('Saved at '+date + 'Data: '+ data);
+               }
+          })
+     }
 })
-record.save(function(err){
-	if(err){
-		console.log(err);
-	}
-	else{
-		var date =new Date();
-		console.log('Saved at '+date);
-	}
-})
+
